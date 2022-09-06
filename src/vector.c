b6cb65ad3f0801a45065e1f609e28883777bba3d
Paul Schulz <paul@mawsonlakes.org>
2022-09-07 08:50:57 +0930

    Enable example text and digital clock display
    Various fonts being used - Courier, Roboto Thin

--- src/vector.c
+++ src/vector.c
@@ -7,14 +7,18 @@
 #define RESOURCE "/org/mawsonlakes/vector.ui"
 
 float m_radius     = 0.42;
-float m_line_width = 0.005;
+float m_line_width = 0.001;
 
 static void
-draw_clock (GtkDrawingArea *area, cairo_t *cr, int width, int height, gpointer user_data) {
+draw_setup (GtkDrawingArea *area,
+            cairo_t *cr,
+            int width,
+            int height,
+            gpointer user_data) {
+
     float line_width;
 
-    // Scale to unit square and translate (0, 0) to be (0.5, 0.5), i.e.
-    // the center of the window
+    // Centre and scale the window
     if (width < height) {
         cairo_scale(cr, width, width);
         line_width = m_line_width * 1000.0 / width;
@@ -27,8 +31,11 @@ draw_clock (GtkDrawingArea *area, cairo_t *cr, int width, int height, gpointer u
     cairo_translate(cr, 0.5, 0.5);
 
     // Set the line width and save the cairo drawing state.
-    cairo_set_line_width(cr, m_line_width);
-    cairo_save(cr);
+    cairo_set_line_width(cr, line_width);
+}
+
+static void
+draw_clock (GtkDrawingArea *area, cairo_t *cr, int width, int height, gpointer user_data) {
 
     cairo_set_source_rgba(cr, 0.0, 0.0, 0.0, 1.0);
     cairo_paint(cr);
@@ -36,7 +43,6 @@ draw_clock (GtkDrawingArea *area, cairo_t *cr, int width, int height, gpointer u
     // Resore back to precious drawing state and draw the circular path
     // representing the clockface. Save this state (including the path) so we
     // can reuse it.
-    cairo_restore(cr);
     cairo_set_source_rgba(cr, 1.0, 1.0, 1.0, 0.7);
     cairo_arc(cr, 0.0, 0.0, m_radius, 0.0, 2.0 * M_PI);
     cairo_save(cr);
@@ -45,7 +51,7 @@ draw_clock (GtkDrawingArea *area, cairo_t *cr, int width, int height, gpointer u
     cairo_restore(cr); /* stack-pen-size */
 
     // Clock ticks
-    for (int i = 0; i < 24; i++)
+    for (int i = 0; i < 60; i++)
     {
         // Major tick size
         double inset = 0.06;
@@ -55,46 +61,30 @@ draw_clock (GtkDrawingArea *area, cairo_t *cr, int width, int height, gpointer u
         cairo_save(cr);
         cairo_set_line_cap(cr, CAIRO_LINE_CAP_ROUND);
 
+        float_t angle;
+
+        angle = 2.0 * M_PI / 60.0;
+
         // Minor ticks are shorter, and narrower.
-        if(i % 3 != 0)
+        if(i % 5 != 0)
         {
-            inset *= 0.8;
-            cairo_set_line_width(cr, m_line_width);
+            inset *= 0.5;
+            // cairo_set_line_width(cr, m_line_width);
         }
 
         // Draw tick mark
         cairo_move_to(
             cr,
-            (m_radius - inset) * cos (i * M_PI / 12.0),
-            (m_radius - inset) * sin (i * M_PI / 12.0));
+            (m_radius - inset) * cos (i * angle),
+            (m_radius - inset) * sin (i * angle));
         cairo_line_to(
             cr,
-            m_radius * cos (i * M_PI / 12.0),
-            m_radius * sin (i * M_PI / 12.0));
+            m_radius * cos (i * angle),
+            m_radius * sin (i * angle));
 
         cairo_stroke(cr);
         cairo_restore(cr); /* stack-pen-size */
 
-        if (true) {
-            cairo_save(cr);
-            char text[256];
-            cairo_select_font_face(cr, "Courier",
-                                   CAIRO_FONT_SLANT_NORMAL,
-                                   CAIRO_FONT_WEIGHT_BOLD);
-            cairo_set_font_size(cr, 15);
-            cairo_set_source_rgb (cr, 1.0, 1.0, 1.0);
-
-            snprintf(text, 256, "%d", i);
-            //cairo_move_to(cr,
-            //             (m_radius - 2 * inset) * cos (i * M_PI / 12.0),
-            //              (m_radius - 2 * inset) * sin (i * M_PI / 12.0));
-            //cairo_show_text(cr, text);
-            cairo_move_to(cr, 0.0,0.0);
-            cairo_show_text(cr, "*");
-            //cairo_stroke(cr);
-            cairo_restore(cr); /* stack-pen-size */
-            cairo_stroke(cr);
-        }
     }
 
     // Draw the analog hands
@@ -121,7 +111,7 @@ draw_clock (GtkDrawingArea *area, cairo_t *cr, int width, int height, gpointer u
 
     // Draw the seconds hand
     // cairo_set_line_width(cr, m_line_width / 3.0);
-    cairo_set_line_width(cr, m_line_width);
+    // cairo_set_line_width(cr, m_line_width);
     // cairo_set_source_rgba(cr, 0.7, 0.7, 0.7, 0.8);   // gray
     cairo_set_source_rgba(cr, 1.0, 1.0, 1.0, 0.7);
     cairo_move_to(cr, 0.0, 0.0);
@@ -151,7 +141,65 @@ draw_clock (GtkDrawingArea *area, cairo_t *cr, int width, int height, gpointer u
     cairo_restore(cr);
 }
 
+//////////////////////////////////////////////////////////////////////////////
+static void
+draw_digital_clock (GtkDrawingArea *area,
+                    cairo_t *cr,
+                    int width,
+                    int height,
+                    gpointer user_data) {
 
+    int x = 0.5;
+    int y = 0.0;
+    float linewidth = 0.1;
+
+    time_t rawtime;
+    time(&rawtime);
+
+    struct tm *timeinfo = localtime (&rawtime);
+
+    char text[256];
+    cairo_select_font_face(cr, "Courier",
+                           CAIRO_FONT_SLANT_NORMAL,
+                           CAIRO_FONT_WEIGHT_BOLD);
+    cairo_set_font_size(cr, linewidth);
+    cairo_set_source_rgb (cr, 1.0, 1.0, 1.0);
+
+    snprintf(text, 256, "%02d:%02d:%02d",
+             timeinfo->tm_hour,
+             timeinfo->tm_min,
+             timeinfo->tm_sec);
+    cairo_move_to(cr, -0.45, -0.4);
+    cairo_show_text(cr, text);
+
+
+
+    char *unit = "Text goes here!";
+
+    cairo_select_font_face (cr, "Roboto Thin",
+                            CAIRO_FONT_SLANT_NORMAL,
+                            CAIRO_FONT_WEIGHT_NORMAL);
+    cairo_set_font_size (cr, linewidth);
+    cairo_set_source_rgb (cr, 1.0, 1.0, 1.0);
+    // cairo_text_extents (cr, unit, &extents);
+    // x = width/2.0-(extents.width/2 + extents.x_bearing);
+    // y = height/2.0 - (extents.height/2 );
+    cairo_move_to (cr, x, y);
+    cairo_show_text (cr, unit);
+
+}
+
+//////////////////////////////////////////////////////////////////////////////
+static void
+draw_display (GtkDrawingArea *area, cairo_t *cr, int width, int height, gpointer user_data) {
+
+    draw_setup (area, cr, width, height, user_data);
+    draw_clock (area, cr, width, height, user_data);
+    draw_digital_clock (area, cr, width, height, user_data);
+
+}
+
+//////////////////////////////////////////////////////////////////////////////
 gboolean
 time_handler(GtkWidget* widget) {
     gtk_widget_queue_draw(widget);
@@ -159,22 +207,21 @@ time_handler(GtkWidget* widget) {
     return TRUE;
 }
 
-
 static void
 app_activate (GApplication *app, gpointer user_data) {
     GtkWidget *win;
-    GtkWidget *clock;
+    GtkWidget *display;
     GtkBuilder *build;
 
     build = gtk_builder_new_from_resource (RESOURCE);
     win = GTK_WIDGET (gtk_builder_get_object (build, "win"));
     gtk_window_set_application (GTK_WINDOW (win), GTK_APPLICATION (app));
 
-    clock = GTK_WIDGET (gtk_builder_get_object (build, "display"));
+    display = GTK_WIDGET (gtk_builder_get_object (build, "display"));
     g_object_unref(build);
 
-    gtk_drawing_area_set_draw_func(GTK_DRAWING_AREA (clock), draw_clock, NULL, NULL);
-    g_timeout_add(1000, (GSourceFunc) time_handler, (gpointer) clock);
+    gtk_drawing_area_set_draw_func(GTK_DRAWING_AREA (display), draw_display, NULL, NULL);
+    g_timeout_add(1000, (GSourceFunc) time_handler, (gpointer) display);
     gtk_widget_show(win);
 
 }
@@ -189,7 +236,7 @@ main (int argc, char **argv) {
     GtkApplication *app;
     int stat;
 
-    app = gtk_application_new ("com.github.ToshioCP.tfc", G_APPLICATION_HANDLES_OPEN);
+    app = gtk_application_new ("com.mawsonlakes.vector", G_APPLICATION_HANDLES_OPEN);
     g_signal_connect (app, "activate", G_CALLBACK (app_activate), NULL);
     g_signal_connect (app, "open", G_CALLBACK (app_open), NULL);
     stat = g_application_run (G_APPLICATION (app), argc, argv);
